40451b8341d3fd4a0f80e9619a514252c503a600
omer.ceder <omerceder@gmail.com>
2024-10-11 13:24:20 +0300

    numbers section ready

--- script.js
+++ script.js
@@ -26,4 +26,36 @@ document.querySelectorAll('.js-lottie-player').forEach(player => {
             player.stop();
         });
     }
+})
+
+// Numeral animation
+document.querySelectorAll('.js-numeral').forEach(numeral => {
+    const initialText = numeral.textContent;
+    numeral.textContent = '0';
+
+    const targetNumber = parseInt(initialText.replace(/,/g, ''), 10);
+    const duration = 1500; // Duration of the animation in milliseconds
+    const frameRate = 30; // Number of frames per second
+    const totalFrames = Math.round(duration / (1000 / frameRate));
+    let frame = 0;
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const counter = setInterval(() => {
+                    frame++;
+                    const progress = frame / totalFrames;
+                    const currentNumber = Math.round(targetNumber * progress);
+                    numeral.textContent = currentNumber.toLocaleString();
+
+                    if (frame === totalFrames) {
+                        clearInterval(counter);
+                    }
+                }, 1000 / frameRate);
+                observer.unobserve(numeral);
+            }
+        });
+    });
+
+    observer.observe(numeral);
 });
